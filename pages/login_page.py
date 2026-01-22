7cbb59c8e25be27e331f4bfe6044abaf9e79d2b6
Nazar Yaremchuk <nazzik173@gmail.com>
2026-01-22 12:57:33 +0200

    feat: add LoginPage with positive and negative login tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,20 @@
 from pages.base_page import BasePage
 
-
 class LoginPage(BasePage):
-    URL = "https://www.saucedemo.com/"
+    URL = "https://www.saucedemo.com"
 
     USERNAME_INPUT = "#user-name"
     PASSWORD_INPUT = "#password"
     LOGIN_BUTTON = "#login-button"
+    ERROR_MESSAGE = "[data-test='error']"
 
     def open(self):
-        self.navigate(self.URL)
+        self.page.goto(self.URL)
 
     def login(self, username: str, password: str):
         self.page.fill(self.USERNAME_INPUT, username)
         self.page.fill(self.PASSWORD_INPUT, password)
         self.page.click(self.LOGIN_BUTTON)
-        
-    
-    def get_error_text(self):
-        return self.page.text_content(".error-message-container")
+
+    def get_error_message(self) -> str:
+        return self.page.text_content(self.ERROR_MESSAGE)
--- tests/test_login.py
+++ tests/test_login.py
@@ -8,3 +8,12 @@ def test_successful_login(page):
     login_page.login("standard_user", "secret_sauce")
 
     assert "inventory" in page.url
+
+def test_login_with_invalid_credentials(page):
+    login_page = LoginPage(page)
+
+    login_page.open()
+    login_page.login("invalid_user", "wrong_password")
+
+    error = login_page.get_error_message()
+    assert "Username and password do not match" in error
